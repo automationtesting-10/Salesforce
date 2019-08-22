76b0e4d258e5b105bcabd3be6f98e89a8f930975
Alejandro Sánchez Luizaga <sanchezluizaga@gmail.com>
2019-08-22 06:47:33 -0400

    Add documentation

--- src/main/java/com/foundation/salesforce/core/utils/EventLogger.java
+++ src/main/java/com/foundation/salesforce/core/utils/EventLogger.java
@@ -16,7 +16,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
- * EventLogger
+ * EventLogger performs basic logging operations, and level setup.
  *
  * @author Alejandro Sánchez Luizaga
  * @version 1.0
@@ -24,46 +24,111 @@ import org.apache.log4j.Logger;
 public class EventLogger {
     private static Logger logger = Logger.getLogger(EventLogger.class.getName());
 
+    /**
+     * Set the level of this Category.
+     *
+     * @param level A level recognized by the system, that is OFF, FATAL, ERROR, WARN, INFODEBUG and ALL.
+     */
     public static void setLevel(Level level) {
         logger.setLevel(level);
     }
 
+    /**
+     * Logs a message object with the DEBUG level.
+     *
+     * @param message
+     */
     public static void debug(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs a message at the DEBUG level including the stack trace of the Throwable t passed as parameter.
+     *
+     * @param message
+     * @param t  An instance of Exception class (or one of its subclasses) thrown by the Java Virtual Machine or
+     *           by the Java throw statement.
+     */
     public static void debug(String message, Throwable t) {
         logger.info(message, t);
     }
 
+    /**
+     * Logs a message object with the INFO level.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     */
     public static void info(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs a message at the INFO level including the stack trace of the Throwable t passed as parameter.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     * @param t  An instance of Exception class (or one of its subclasses) thrown by the Java Virtual Machine or
+     *           by the Java throw statement.
+     */
     public static void info(String message, Throwable t) {
         logger.info(message, t);
     }
 
+    /**
+     * Logs a message object with the WARN level.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     */
     public static void warn(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs a message at the WARN level including the stack trace of the Throwable t passed as parameter.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     * @param t  An instance of Exception class (or one of its subclasses) thrown by the Java Virtual Machine or
+     *           by the Java throw statement.
+     */
     public static void warn(String message, Throwable t) {
         logger.info(message, t);
     }
 
+    /**
+     * Logs a message object with the ERROR level.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     */
     public static void error(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs a message at the ERROR level including the stack trace of the Throwable t passed as parameter.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     * @param t  An instance of Exception class (or one of its subclasses) thrown by the Java Virtual Machine or
+     *           by the Java throw statement.
+     */
     public static void error(String message, Throwable t) {
         logger.info(message, t);
     }
 
+    /**
+     * Logs a message object with the FATAL level.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     */
     public static void fatal(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs a message at the FATAL level including the stack trace of the Throwable t passed as parameter.
+     *
+     * @param message A customized message that briefly summarizes the situation.
+     * @param t  An instance of Exception class (or one of its subclasses) thrown by the Java Virtual Machine or
+     *           by the Java throw statement.
+     */
     public static void fatal(String message, Throwable t) {
         logger.info(message, t);
     }
