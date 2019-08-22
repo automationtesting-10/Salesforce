fc689d474e3bf63e9fc1a2deb7841c11f76005d4
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-08-22 03:15:31 -0400

    Add new class for the steps to feachures.

---
+++ src/test/java/com/foundation/salesforce/steps/ContactAcceptanceSteps.java
@@ -0,0 +1,26 @@
+/*
+ * @(#) ContactAcceptanceSteps.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av. Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+package com.foundation.salesforce.steps;
+
+import cucumber.api.java.en.Given;
+
+/**
+ * Class where it is stored the steps for the feachure are carried out.
+ *
+ * @author Jesus Menacho
+ * @version 1.0
+ */
+public class ContactAcceptanceSteps {
+    @Given("a user sing in the web page SalesForce")
+    public void autheticationUser(){
+    }
+}
