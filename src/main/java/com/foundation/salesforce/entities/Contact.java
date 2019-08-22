980179a7b0f0fb80a5ebae0aea9e2ddda6ef7975
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-08-22 03:12:35 -0400

    Add new class when save values client.

---
+++ src/main/java/com/foundation/salesforce/entities/Contact.java
@@ -0,0 +1,57 @@
+/*
+ * @(#) Contact.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av. Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+package com.foundation.salesforce.entities;
+
+/**
+ * Class where the values of the fields required to create a sales activity are stored.
+ *
+ * @author Jesus Menacho
+ * @version 1.0
+ */
+public class Contact {
+    String lastName;
+    String fistName;
+    String email;
+    String Title;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+}
