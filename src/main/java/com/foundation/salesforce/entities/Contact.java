8c867f633892d37ed8ea929bbdaa0c0e3ef90caa
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-08-23 16:25:35 -0400

    Add the documentations

--- src/main/java/com/foundation/salesforce/entities/Contact.java
+++ src/main/java/com/foundation/salesforce/entities/Contact.java
@@ -23,34 +23,74 @@ public class Contact {
     String email;
     String Title;
 
+    /**
+     * Method to return the lasName from the user.
+     *
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Method to let lastName from the user.
+     *
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Method to let the return the firstName from the first name.
+     *
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Method to let the firstName to the user.
+     *
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Method to let get the email form the user.
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Method to let get a email from the user.
+     *
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Method to let return the title to the user.
+     *
+     * @return Title.
+     */
     public String getTitle() {
         return Title;
     }
 
+    /**
+     * Method to let a title to the user.
+     *
+     * @param title
+     */
     public void setTitle(String title) {
         Title = title;
     }
