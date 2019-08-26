e65247aa6a5e43e415341bcbd6ac4975b4465aeb
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-08-26 14:53:47 -0400

    Add space in to the imports

--- src/test/java/com/foundation/salesforce/steps/ContactSteps.java
+++ src/test/java/com/foundation/salesforce/steps/ContactSteps.java
@@ -9,16 +9,20 @@
  * accordance with the terms of the license agreement you entered into
  * with Jala Foundation.
  */
+
 package com.foundation.salesforce.steps;
 
 import com.foundation.salesforce.core.api.ContactApi;
 import com.foundation.salesforce.core.restClient.RestClientApi;
 import com.foundation.salesforce.core.utils.EndPoints;
 import com.foundation.salesforce.entities.Contact;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 import io.restassured.response.Response;
+
 import org.testng.Assert;
 
 import java.util.Map;
