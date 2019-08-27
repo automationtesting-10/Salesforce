1556594816b3c935e2e810bf2412d594ca8181f3
Cxristian <cxristian.lujan05@gmail.com>
2019-08-27 13:57:18 -0400

    refactor features

--- src/test/java/com/foundation/salesforce/runner/RunCukesTest.java
+++ src/test/java/com/foundation/salesforce/runner/RunCukesTest.java
@@ -30,7 +30,7 @@ import org.testng.annotations.AfterTest;
                 "json:target/cucumber.json",
                 "rerun:target/rerun.txt"},
         glue = {"com/foundation/salesforce/steps", "com/foundation/salesforce/hooks"},
-        features = {"src/test/resources/features/opportunity"},
+        features = {"src/test/resources/features/"},
         monochrome = true)
 public class RunCukesTest extends AbstractTestNGCucumberTests {
 
