2e368dc7e79801b10915650a2348110229467f66
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-08-27 20:35:32 -0400

    Code review fixes

--- src/test/java/com/foundation/salesforce/steps/LeadsSteps.java
+++ src/test/java/com/foundation/salesforce/steps/LeadsSteps.java
@@ -13,24 +13,20 @@ package com.foundation.salesforce.steps;
 
 import com.foundation.salesforce.core.restClient.RestClientApi;
 import com.foundation.salesforce.core.utils.EndPoints;
-import com.foundation.salesforce.core.utils.ResponseValidation;
 import com.foundation.salesforce.entities.Context;
 import com.foundation.salesforce.core.restClient.Authentication;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.json.JSONObject;
+
 import org.testng.Assert;
-import org.testng.asserts.SoftAssert;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static io.restassured.RestAssured.given;
 
 /**
  * LeadsSteps class contains steps for lead endpoint.
